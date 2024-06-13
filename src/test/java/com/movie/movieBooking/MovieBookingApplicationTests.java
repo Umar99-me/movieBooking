87130e0716d784c65fddd4b5a4c06558b40151f0
Umar99-me <umarsaleem99999@gmail.com>
2024-06-13 20:56:12 +0530

    Intial Project setUp

---
+++ src/test/java/com/movie/movieBooking/MovieBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.movieBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
